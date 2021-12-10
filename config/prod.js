95c098bd4d266ad14a4041c5e0c368eb859dba3a
koren3210 <koren3210@gmail.com>
2021-12-10 16:05:59 +0200

    g

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://pavel:Pv123456@cluster0.bgitu.mongodb.net/CORRELLO_DB?retryWrites=true&w=majority',
+  'dbURL2c': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
 }
